6275ca6498ff82c508598180e69962f42938d082
Jean-Emrick Rioux <gestionjerioux@gmail.com>
2022-04-19 00:00:37 -0400

    loading snack component

---
+++ src/components/LoadingSnack.vue
@@ -0,0 +1,39 @@
+<template>
+  <v-snackbar
+      :model-value="!isLoaded"
+      color="primary"
+      timeout="-1"
+      variant="contained"
+    >
+   
+
+    <div class="text-subtitle2">
+      <v-progress-circular
+      indeterminate
+      color="white"
+      class="mr-3"
+      size="20"
+    ></v-progress-circular>
+      Fetching {{what }}... Please wait.
+      </div>
+    </v-snackbar>
+</template>
+
+<script>
+export default {
+  name: "LoadingSnack",
+  data: () => ({}),
+  methods: {},
+  computed: {},
+  props: {
+    isLoaded: {
+      type: Boolean,
+      required: true,
+    },
+    what: {type: String}
+  },
+};
+</script>
+
+<!-- Add "scoped" attribute to limit CSS to this component only -->
+<style scoped></style>
