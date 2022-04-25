e58da4b9d4d77c7cb9a39a49ef95e47f1bb049c6
Jean-Emrick Rioux <gestionjerioux@gmail.com>
2022-04-24 23:07:46 -0400

    adjust card actions for release

--- src/components/NetworkSummary.vue
+++ src/components/NetworkSummary.vue
@@ -36,110 +36,13 @@
         </div>
       </v-card-text>
       <v-divider></v-divider>
-      <v-card-actions class="vcard_action d-flex justify-space-around">
-        <v-btn class="vcard_action_btn" text>See Mor <v-icon class="vcard_action_icon ml-1" icon="mdi-information-outline"></v-icon></v-btn>
-        <v-btn v-if="balancesLoaded" class="vcard_action_btn" text>{{ parseFloat(balances(network.name).total).toFixed(2) }} {{network.symbol}}</v-btn>
+      <v-card-actions class="vcard_action d-flex justify-space-between">
+        <!-- <v-btn class="vcard_action_btn" text>See More</v-btn> -->
+        <div>🥩 🍟</div>
+        <v-btn v-if="balancesLoaded" class="vcard_action_btn" text><div class="text-body-1">{{ parseFloat(balances(network.name).total).toFixed(2) }} {{network.symbol}}</div></v-btn>
       </v-card-actions>
     </v-card>
   </v-col>
-<!-- END network info vue -->
-  <!-- OG network info vue -->
-  <!-- <v-col xs="12" sm="6" md="6">
-    <v-card dark variant="outlined" :loading="networksLoaded">
-      <v-card-title>
-        <v-avatar size="50" class="mr-3">
-          <img height="35" :src="image(network.name)" />
-        </v-avatar>
-        <strong>{{ network.name }}</strong>
-        <span v-if="pricesLoaded">
-          - {{ price(network.name) }} ${{currency.text}}</span
-        ></v-card-title
-      >
-      <v-expansion-panels>
-        <v-expansion-panel elevation="0">
-          <v-expansion-panel-title>Network info</v-expansion-panel-title>
-          <v-expansion-panel-text>
-            <v-table density="compact">
-              <tbody v-if="networksLoaded">
-                <tr>
-                  <td><strong>Chain ID</strong></td>
-                  <td>{{ network.chainId }}</td>
-                </tr>
-                <tr>
-                  <td><strong>Denom</strong></td>
-                  <td>{{ network.denom }}</td>
-                </tr>
-                <tr>
-                  <td><strong>Coin Gecko ID</strong></td>
-                  <td>{{ network.coinGeckoId }}</td>
-                </tr>
-                <tr>
-                  <td><strong>APY</strong></td>
-                  <td>{{ apy }}</td>
-                </tr>
-                <tr>
-                  <td><strong>Symbol</strong></td>
-                  <td>${{ network.symbol }}</td>
-                </tr>
-              </tbody>
-              <tbody v-else>
-                <tr>
-                  <td colspan="2">
-                    <v-progress-linear
-                      indeterminate
-                      color="primary"
-                    ></v-progress-linear>
-                  </td>
-                </tr>
-              </tbody>
-            </v-table>
-          </v-expansion-panel-text>
-        </v-expansion-panel>
-        <v-expansion-panel elevation="0">
-          <v-expansion-panel-title>Balances</v-expansion-panel-title>
-          <v-expansion-panel-text>
-            <v-table v-if="balancesLoaded" density="compact" >
-              <tbody>
-                <tr>
-                  <td><strong>Total</strong></td>
-                  <td>{{ balances(network.name).total }}</td>
-                </tr>
-                <tr>
-                  <td><strong>Liquid</strong></td>
-                  <td>{{ balances(network.name).liquid }}</td>
-                </tr>
-                <tr>
-                  <td><strong>Staked</strong></td>
-                  <td>{{ balances(network.name).staked }}</td>
-                </tr>
-                <tr>
-                  <td><strong>Rewards</strong></td>
-                  <td>{{ balances(network.name).rewards }}</td>
-                </tr>
-                <tr>
-                  <td><strong>VALUE $</strong></td>
-                  <td>{{ balances(network.name).value }}</td>
-                </tr>
-              </tbody>
-            </v-table>
-            <v-table v-else>
-              <tbody>
-                <tr>
-                  <td colspan="2">
-                    <v-progress-linear
-                      indeterminate
-                      color="primary"
-                    ></v-progress-linear>
-                  </td>
-                </tr>
-              </tbody>
-            </v-table>
-          </v-expansion-panel-text>
-        </v-expansion-panel>
-      </v-expansion-panels>
-    </v-card>
-  </v-col> -->
-  <!-- END OG networkinfo vue -->
 </template>
 
 <script>
