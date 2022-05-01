905b05fcb9c8b0477ea5dd2d178fd338a0d2390c
Jean-Emrick Rioux <gestionjerioux@gmail.com>
2022-05-01 13:24:33 -0400

    formatting

--- src/components/ProposalList.vue
+++ src/components/ProposalList.vue
@@ -1,61 +1,61 @@
 <template>
-  <v-list-item variant="contained">
-    <v-list-item-avatar left>
-      <v-chip class="ma-2" :color="proposalColor" label text-color="white">
-        <b>{{ proposal.proposalId.low }}</b>
-      </v-chip>
-    </v-list-item-avatar>
-    <v-list-item-header>
-      <v-list-item-title>{{ content.title }}</v-list-item-title>
-      <v-list-item-subtitle>{{ type }}</v-list-item-subtitle>
-    </v-list-item-header>
-    <v-btn
-      v-if="statusRaw == 2"
-      class="mr-3"
-      color="primary"
-      variant="outlined"
-    >
-      Vote
-      <v-dialog v-model="dialog" activator="parent">
-        <v-card>
-          <v-card-text> Cast your vote </v-card-text>
-          <v-card-text>
-            <v-radio-group v-model="vote" mandatory>
-              <v-radio label="Yes" :value="1" v-model="vote"></v-radio>
-              <v-radio label="No" :value="3" v-model="vote"></v-radio>
-              <v-radio label="No With Veto" :value="4" v-model="vote"></v-radio>
-              <v-radio label="Abstain" :value="2" v-model="vote"></v-radio>
-            </v-radio-group>
-          </v-card-text>
+	<v-list-item variant="contained">
+		<v-list-item-avatar left>
+			<v-chip class="ma-2" :color="proposalColor" label text-color="white">
+				<b>{{ proposal.proposalId.low }}</b>
+			</v-chip>
+		</v-list-item-avatar>
+		<v-list-item-header>
+			<v-list-item-title>{{ content.title }}</v-list-item-title>
+			<v-list-item-subtitle>{{ type }}</v-list-item-subtitle>
+		</v-list-item-header>
+		<v-btn
+			v-if="statusRaw == 2"
+			class="mr-3"
+			color="primary"
+			variant="outlined"
+		>
+			Vote
+			<v-dialog v-model="dialog" activator="parent">
+				<v-card>
+					<v-card-text> Cast your vote </v-card-text>
+					<v-card-text>
+						<v-radio-group v-model="vote" mandatory>
+							<v-radio label="Yes" :value="1" v-model="vote"></v-radio>
+							<v-radio label="No" :value="3" v-model="vote"></v-radio>
+							<v-radio label="No With Veto" :value="4" v-model="vote"></v-radio>
+							<v-radio label="Abstain" :value="2" v-model="vote"></v-radio>
+						</v-radio-group>
+					</v-card-text>
 
-          <v-card-actions>
-            <v-btn
-              @click="castVote()"
-              variant="contained-text"
-              color="primary"
-              size="large"
-            >
-              Vote
-            </v-btn>
-            <v-btn @click="closeDialog()"> Close Dialog </v-btn>
-          </v-card-actions>
-        </v-card>
-      </v-dialog>
-    </v-btn>
-    <v-btn @click="expand = !expand" variant="outlined">
-      <v-icon>mdi-eye</v-icon>
-    </v-btn>
-  </v-list-item>
-  <v-expand-transition>
-    <div v-if="expand">
-      <v-list>
-        <v-list-item>{{ fullDescription }}</v-list-item>
-        <v-list-item v-if="isDone">
-          <BarChart :results="proposal.finalTallyResult" />
-        </v-list-item>
-      </v-list>
-    </div>
-  </v-expand-transition>
+					<v-card-actions>
+						<v-btn
+							@click="castVote()"
+							variant="contained-text"
+							color="primary"
+							size="large"
+						>
+							Vote
+						</v-btn>
+						<v-btn @click="closeDialog()"> Close Dialog </v-btn>
+					</v-card-actions>
+				</v-card>
+			</v-dialog>
+		</v-btn>
+		<v-btn @click="expand = !expand" variant="outlined">
+			<v-icon>mdi-eye</v-icon>
+		</v-btn>
+	</v-list-item>
+	<v-expand-transition>
+		<div v-if="expand">
+			<v-list>
+				<v-list-item>{{ fullDescription }}</v-list-item>
+				<v-list-item v-if="isDone">
+					<BarChart :results="proposal.finalTallyResult" />
+				</v-list-item>
+			</v-list>
+		</div>
+	</v-expand-transition>
 </template>
 
 <script>
@@ -69,174 +69,174 @@ import { ParameterChangeProposal } from "cosmjs-types/cosmos/params/v1beta1/para
 import { SoftwareUpgradeProposal } from "cosmjs-types/cosmos/upgrade/v1beta1/upgrade";
 import { CommunityPoolSpendProposal } from "cosmjs-types/cosmos/distribution/v1beta1/distribution";
 import {
-  defaultRegistryTypes as defaultStargateTypes,
-  SigningStargateClient,
+	defaultRegistryTypes as defaultStargateTypes,
+	SigningStargateClient,
 } from "@cosmjs/stargate";
 
 const proposalStatus = (status) => {
-  switch (status) {
-    case 1:
-      return "Deposit";
-      break;
-    case 2:
-      return "Voting";
-      break;
-    case 3:
-      return "Passed";
-      break;
-    case 4:
-      return "Rejected";
-      break;
-    case 5:
-      return "Failed";
-      break;
-    default:
-      break;
-  }
+	switch (status) {
+		case 1:
+			return "Deposit";
+			break;
+		case 2:
+			return "Voting";
+			break;
+		case 3:
+			return "Passed";
+			break;
+		case 4:
+			return "Rejected";
+			break;
+		case 5:
+			return "Failed";
+			break;
+		default:
+			break;
+	}
 };
 
 const proposalColor = (status) => {
-  switch (status) {
-    case 1:
-      return "green";
-      break;
-    case 2:
-      return "blue";
-      break;
-    case 3:
-      return "green";
-      break;
-    case 4:
-      return "red";
-      break;
-    case 5:
-      return "red";
-      break;
-    default:
-      break;
-  }
+	switch (status) {
+		case 1:
+			return "green";
+			break;
+		case 2:
+			return "blue";
+			break;
+		case 3:
+			return "green";
+			break;
+		case 4:
+			return "red";
+			break;
+		case 5:
+			return "red";
+			break;
+		default:
+			break;
+	}
 };
 
 const proposalType = (type) => {
-  switch (type) {
-    case "/cosmos.params.v1beta1.ParameterChangeProposal":
-      return "Parameter Change";
-      break;
-    case "/cosmos.distribution.v1beta1.CommunityPoolSpendProposal":
-      return "Community Pool Spend";
-      break;
-    case "/cosmos.gov.v1beta1.TextProposal":
-      return "Text";
-      break;
-    case "/cosmos.gov.v1beta1.SoftwareUpgradeProposal":
-      return "Software Upgrade";
-      break;
-    case "/ibc.core.client.v1.ClientUpdateProposal":
-      return "IBC Client Update";
-      break;
-    default:
-      break;
-  }
+	switch (type) {
+		case "/cosmos.params.v1beta1.ParameterChangeProposal":
+			return "Parameter Change";
+			break;
+		case "/cosmos.distribution.v1beta1.CommunityPoolSpendProposal":
+			return "Community Pool Spend";
+			break;
+		case "/cosmos.gov.v1beta1.TextProposal":
+			return "Text";
+			break;
+		case "/cosmos.gov.v1beta1.SoftwareUpgradeProposal":
+			return "Software Upgrade";
+			break;
+		case "/ibc.core.client.v1.ClientUpdateProposal":
+			return "IBC Client Update";
+			break;
+		default:
+			break;
+	}
 };
 
 const excerpt = (string) => {
-  if (string.length > 350) {
-    return string.substring(0, 350) + "...";
-  } else {
-    return string;
-  }
+	if (string.length > 350) {
+		return string.substring(0, 350) + "...";
+	} else {
+		return string;
+	}
 };
 export default {
-  name: "Proposal",
-  components: { Coin, BarChart },
-  data() {
-    return {
-      dialog: false,
-      vote: 0,
-      expand: false,
-    };
-  },
-  props: {
-    proposal: Object,
-  },
-  methods: {
-    closeDialog() {
-      this.dialog = false;
-      this.vote = 0;
-    },
-    async castVote() {
-      const params = {
-        proposalId: this.$props.proposal.proposalId,
-        option: this.vote,
-        voter: this.$store.state.wallet.address,
-      };
-      console.log(params);
-      await this.$store.dispatch("castVote", params);
-      this.dialog = false;
-      this.vote = "";
-    },
-    ...mapGetters({
-      signingClient: "getSigningClient",
-    }),
-  },
-  computed: {
-    status() {
-      return proposalStatus(this.$props.proposal.status);
-    },
-    proposalColor() {
-      return proposalColor(this.$props.proposal.status);
-    },
-    expansionText() {
-      return this.expand ? "Hide" : "See More";
-    },
-    isDone() {
-      return (
-        this.$props.proposal.status != 1 && this.$props.proposal.status != 2
-      );
-    },
-    statusRaw() {
-      return this.$props.proposal.status;
-    },
-    registry() {
-      const registry = new Registry(defaultStargateTypes);
-      registry.register(
-        "/cosmos.params.v1beta1.ParameterChangeProposal",
-        ParameterChangeProposal
-      );
-      registry.register("/cosmos.gov.v1beta1.TextProposal", TextProposal);
-      registry.register(
-        "/cosmos.upgrade.v1beta1.SoftwareUpgradeProposal",
-        SoftwareUpgradeProposal
-      );
-      registry.register(
-        "/cosmos.distribution.v1beta1.CommunityPoolSpendProposal",
-        CommunityPoolSpendProposal
-      );
-      registry.register(
-        "/ibc.core.client.v1.ClientUpdateProposal",
-        ClientUpdateProposal
-      );
-      return registry;
-    },
-    description() {
-      return excerpt(this.content.description);
-    },
-    fullDescription() {
-      return this.content.description;
-    },
-    content() {
-      return this.registry.decode(this.$props.proposal.content);
-    },
-    type() {
-      return proposalType(this.$props.proposal.content.typeUrl);
-    },
-  },
+	name: "Proposal",
+	components: { Coin, BarChart },
+	data() {
+		return {
+			dialog: false,
+			vote: 0,
+			expand: false,
+		};
+	},
+	props: {
+		proposal: Object,
+	},
+	methods: {
+		closeDialog() {
+			this.dialog = false;
+			this.vote = 0;
+		},
+		async castVote() {
+			const params = {
+				proposalId: this.$props.proposal.proposalId,
+				option: this.vote,
+				voter: this.$store.state.wallet.address,
+			};
+			console.log(params);
+			await this.$store.dispatch("castVote", params);
+			this.dialog = false;
+			this.vote = "";
+		},
+		...mapGetters({
+			signingClient: "getSigningClient",
+		}),
+	},
+	computed: {
+		status() {
+			return proposalStatus(this.$props.proposal.status);
+		},
+		proposalColor() {
+			return proposalColor(this.$props.proposal.status);
+		},
+		expansionText() {
+			return this.expand ? "Hide" : "See More";
+		},
+		isDone() {
+			return (
+				this.$props.proposal.status != 1 && this.$props.proposal.status != 2
+			);
+		},
+		statusRaw() {
+			return this.$props.proposal.status;
+		},
+		registry() {
+			const registry = new Registry(defaultStargateTypes);
+			registry.register(
+				"/cosmos.params.v1beta1.ParameterChangeProposal",
+				ParameterChangeProposal
+			);
+			registry.register("/cosmos.gov.v1beta1.TextProposal", TextProposal);
+			registry.register(
+				"/cosmos.upgrade.v1beta1.SoftwareUpgradeProposal",
+				SoftwareUpgradeProposal
+			);
+			registry.register(
+				"/cosmos.distribution.v1beta1.CommunityPoolSpendProposal",
+				CommunityPoolSpendProposal
+			);
+			registry.register(
+				"/ibc.core.client.v1.ClientUpdateProposal",
+				ClientUpdateProposal
+			);
+			return registry;
+		},
+		description() {
+			return excerpt(this.content.description);
+		},
+		fullDescription() {
+			return this.content.description;
+		},
+		content() {
+			return this.registry.decode(this.$props.proposal.content);
+		},
+		type() {
+			return proposalType(this.$props.proposal.content.typeUrl);
+		},
+	},
 };
 </script>
 
 <style scoped>
 .scroll {
-  max-height: 100px;
-  overflow-y: scroll;
+	max-height: 100px;
+	overflow-y: scroll;
 }
 </style>
--- src/views/DashboardView.vue
+++ src/views/DashboardView.vue
@@ -1,70 +1,128 @@
 <template>
-<!-- <v-alert closable type="info" class="my-5" border>
-            <v-alert-title>
-              <div class="text-subtitle-1">Quick pointers</div>
-              </v-alert-title>
-            <div class="text-body-2">
-              To change the chains that display or the wallets that are tracked you can click on the <v-icon>mdi-account-cog</v-icon> icon in the app bar <br>
-              To refresh prices, you can click on the  <v-icon>mdi-currency-usd</v-icon> icon in the app bar <br>
-              To refresh your balances, you can click on the  <v-icon>mdi-refresh</v-icon> icon in the app bar.<br> This operation can be long as it needs to do 3 different queries on all the chains times the number of wallet you are tracking. <br> <br>
-              Happy tracking sers 🥩 🍟! <br>
-              If you want to help us, consider delegating to our ATOM node <a href="https://restake.app/cosmoshub/cosmosvaloper1uepjmgfuk6rnd0djsglu88w7d0t49lmljdpae2" target="_blank">here</a> ❤️
-            </div>
-          </v-alert> -->
-          <v-row>
-            <v-col>
-              <div class="d-flex justify-space-between">
-                <v-row>
-                  <v-col><div class="text-h1 my-10">Trakfolio</div></v-col>
-                  <v-col v-if="!mobile"><div class="text-h2 my-10 d-flex justify-end">{{value(totalValue.total)}}</div></v-col>
-                  <v-col v-else><div class="text-h2 my-3 d-flex justify-start">{{value(totalValue.total)}}</div></v-col>
-                </v-row>
-              </div>
-            </v-col>
-          </v-row>
-          <v-row v-if="!mobile" >
-              <v-col >
-                <div class="d-flex justify-start">
-                <v-chip-group mandatory v-model="selected">
-                <v-chip size="large" class="mx-1 vchip_card " color="primary" value="All" label>All</v-chip>
-                <v-chip size="large" v-for="wallet in totalValue.wallets" :key="wallet.name" class="mx-1 vchip_card " color="primary" :value="wallet.name" label>{{wallet.name}}</v-chip>
-            <!--  <v-chip class="ma-2 vchip_card " color="primary" value="denom" label>{{network.symbol}}</v-chip>
-              <v-chip class="ma-2 vchip_card" color="primary"  value="base" label>{{currency.text}}</v-chip> -->
-              </v-chip-group>
-              </div>
-            </v-col>
-            <v-col >
-              <div class="d-flex justify-end">
-              <v-chip label class="mx-1" @click="refreshPrices" prepend-icon="mdi-currency-usd" append-icon="mdi-refresh">{{showTime(priceTime)}}</v-chip>
-              <v-chip label class="mx-1" @click="refreshBalances" prepend-icon="mdi-account-cash" append-icon="mdi-refresh">{{showTime(priceTime)}}</v-chip>
-              </div>
-            </v-col>
-          </v-row>
-            <v-row v-else class="d-flex flex-row justify-space-between">
-              <v-col >
-                <v-chip-group  mandatory v-model="selected">
-                <v-chip size="large" class="mx-1 vchip_card " color="primary" value="All" label>All</v-chip>
-                <v-chip size="large" v-for="wallet in totalValue.wallets" :key="wallet.name" class="mx-1 vchip_card " color="primary" :value="wallet.name" label>{{wallet.name}}</v-chip>
-            <!--  <v-chip class="ma-2 vchip_card " color="primary" value="denom" label>{{network.symbol}}</v-chip>
-              <v-chip class="ma-2 vchip_card" color="primary"  value="base" label>{{currency.text}}</v-chip> -->
-              </v-chip-group>
-            </v-col>
-            <v-col  class="d-flex">
-              <v-chip size="large" label class="mx-1" @click="refreshPrices" prepend-icon="mdi-currency-usd" append-icon="mdi-refresh">{{showTime(priceTime)}}</v-chip>
-              <v-chip size="large" label class="mx-1" @click="refreshBalances" prepend-icon="mdi-account-cash" append-icon="mdi-refresh">{{showTime(priceTime)}}</v-chip>
-            </v-col>
-          </v-row>
-          <v-row class="d-flex flex-row">
-            <DashboardSummary :selected="selected"/>
-</v-row>       
-  <v-row>
-    <NetworkSummary
-     :wallet="selected"
-      v-for="network in selectedNetworks"
-      :key="network.name"
-      :network="network"
-    ></NetworkSummary>
-  </v-row>
+	<v-row>
+		<v-col>
+			<div class="d-flex justify-space-between">
+				<v-row>
+					<v-col>
+						<div class="text-h1 my-10">Trakfolio</div>
+					</v-col>
+					<v-col v-if="!mobile">
+						<div class="text-h2 my-10 d-flex justify-end">
+							{{ total(selected) }}
+						</div>
+					</v-col>
+					<v-col v-else>
+						<div class="text-h2 my-3 d-flex justify-start">
+							{{ total(selected) }}
+						</div>
+					</v-col>
+				</v-row>
+			</div>
+		</v-col>
+	</v-row>
+	<v-row v-if="!mobile">
+		<v-col>
+			<div class="d-flex justify-start">
+				<v-chip-group mandatory v-model="selected">
+					<v-chip
+						size="large"
+						class="mx-1 vchip_card"
+						@click="changeValue"
+						color="primary"
+						value="All"
+						label
+						>All</v-chip
+					>
+					<v-chip
+						size="large"
+						v-for="wallet in totalValue.wallets"
+						@click="changeValue"
+						:key="wallet.name"
+						class="mx-1 vchip_card"
+						color="primary"
+						:value="wallet.name"
+						label
+						>{{ wallet.name }}</v-chip
+					>
+				</v-chip-group>
+			</div>
+		</v-col>
+		<v-col>
+			<div class="d-flex justify-end">
+				<v-chip
+					label
+					class="mx-1"
+					@click="refreshPrices"
+					prepend-icon="mdi-currency-usd"
+					append-icon="mdi-refresh"
+					>{{ showTime(priceTime) }}</v-chip
+				>
+				<v-chip
+					label
+					class="mx-1"
+					@click="refreshBalances"
+					prepend-icon="mdi-account-cash"
+					append-icon="mdi-refresh"
+					>{{ showTime(priceTime) }}</v-chip
+				>
+			</div>
+		</v-col>
+	</v-row>
+	<v-row v-else class="d-flex flex-row justify-space-between">
+		<v-col>
+			<v-chip-group mandatory v-model="selected">
+				<v-chip
+					size="large"
+					class="mx-1 vchip_card"
+					color="primary"
+					value="All"
+					label
+					>All</v-chip
+				>
+				<v-chip
+					size="large"
+					v-for="wallet in totalValue.wallets"
+					:key="wallet.name"
+					class="mx-1 vchip_card"
+					color="primary"
+					:value="wallet.name"
+					label
+					>{{ wallet.name }}</v-chip
+				>
+			</v-chip-group>
+		</v-col>
+		<v-col class="d-flex">
+			<v-chip
+				size="large"
+				label
+				class="mx-1"
+				@click="refreshPrices"
+				prepend-icon="mdi-currency-usd"
+				append-icon="mdi-refresh"
+				>{{ showTime(priceTime) }}</v-chip
+			>
+			<v-chip
+				size="large"
+				label
+				class="mx-1"
+				@click="refreshBalances"
+				prepend-icon="mdi-account-cash"
+				append-icon="mdi-refresh"
+				>{{ showTime(priceTime) }}</v-chip
+			>
+		</v-col>
+	</v-row>
+	<v-row class="d-flex flex-row">
+		<DashboardSummary :selected="selected" />
+	</v-row>
+	<v-row>
+		<NetworkSummary
+			:wallet="selected"
+			v-for="network in selectedNetworks"
+			:key="network.name"
+			:network="network"
+		></NetworkSummary>
+	</v-row>
 </template>
 
 <script>
@@ -74,71 +132,100 @@ import NetworkSummary from "@/components/NetworkSummary.vue";
 import PieChart from "@/components/BarChart.vue";
 import AmountsCard from "@/components/AmountsCard.vue";
 import DashboardSummary from "@/components/DashboardSummary.vue";
+import _ from "lodash";
 export default {
-  name: "DashboardView",
-  components: { NetworkSummary, PieChart, AmountsCard, DashboardSummary },
-  data() {
-    return {
-      model: [],
-      selected: "All" ,
-    }
-  },
-  async created() {
-    if (this.seedAddresses.length === 0 || this.selectedNetworks.length === 0) {
-      this.$router.push("/select");
-    }
-  },
-  methods: {
-    ...mapActions({
-      fetchNetworks: "fetchNetworks",
-      setAlert: "setDashboardAlert",
-       refreshPrices: "refreshPrices",
-      refreshBalances: "refreshBalances",
-      changeSelectedWallet: "changeSelectedWallet",
-    }),
-    showTime(time) {
-      const unixStamp = time - (1000 * 60 * 60)
-      return this.mobile ?  moment(parseInt(unixStamp)).format("HH:mm"): moment(parseInt(unixStamp)).format("DD/MM HH:mm");
-    },
-        value(amount) {
-          let locale = "en-US";
-          if (this.currency.value === "cad") {
-            locale = "fr-CA";
-          } else if (this.currency.value === "eur") {
-            locale = "fr-FR";
-          }
-          let intl = new Intl.NumberFormat(locale, {
-            style: "currency",
-            currency: this.currency.value,
-            minimumFractionDigits: 2,
-          });
-      return intl.format(amount);
-    },
-  },
-  computed: {
-    ...mapGetters({
-      selectedNetworks: "getSelectedNetworks",
-      totalValue: "getTotalValue",
-         currency: "getCurrency",
-      balances: "getBalancesByName",
-      networksLoaded: "getIsNetworksLoaded",
-      balancesLoaded: "getIsBalancesLoaded",
-      seedAddresses : "getSeedAddresses",
-      showAlert: "getDashboardAlert",
-      balanceTime: "getBalanceExpireTime",
-      priceTime: "getLastPriceTime",
-      selectedWallet: "getSelectedWallet",
-    }),
-     mobile() {
-       console.log(this.$vuetify.display.sm)
-    return this.$vuetify.display.xs || this.$vuetify.display.sm;
-  },
-  },
+	name: "DashboardView",
+	components: { NetworkSummary, PieChart, AmountsCard, DashboardSummary },
+	data() {
+		return {
+			model: [],
+			selected: "All",
+		};
+	},
+	async created() {
+		if (this.seedAddresses.length === 0 || this.selectedNetworks.length === 0) {
+			this.$router.push("/select");
+		}
+	},
+	methods: {
+		changeValue() {
+			this.changeSelectedWallet(this.selected);
+		},
+		total(filter) {
+			if (filter === "All") {
+				return this.value(this.totalValue.total);
+			} else {
+				return this.value(
+					_.keyBy(this.totalValue.wallets, "name")[filter].total
+				);
+			}
+		},
+		value(amount) {
+			let locale = "en-US";
+			if (this.currency.value === "cad") {
+				locale = "fr-CA";
+			} else if (this.currency.value === "eur") {
+				locale = "fr-FR";
+			}
+			let intl = new Intl.NumberFormat(locale, {
+				style: "currency",
+				currency: this.currency.value,
+				minimumFractionDigits: 2,
+			});
+			return intl.format(amount);
+		},
+		...mapActions({
+			fetchNetworks: "fetchNetworks",
+			setAlert: "setDashboardAlert",
+			refreshPrices: "refreshPrices",
+			refreshBalances: "refreshBalances",
+			changeSelectedWallet: "changeSelectedWallet",
+		}),
+		showTime(time) {
+			const unixStamp = time - 1000 * 60 * 60;
+			return this.mobile
+				? moment(parseInt(unixStamp)).format("HH:mm")
+				: moment(parseInt(unixStamp)).format("DD/MM HH:mm");
+		},
+		value(amount) {
+			let locale = "en-US";
+			if (this.currency.value === "cad") {
+				locale = "fr-CA";
+			} else if (this.currency.value === "eur") {
+				locale = "fr-FR";
+			}
+			let intl = new Intl.NumberFormat(locale, {
+				style: "currency",
+				currency: this.currency.value,
+				minimumFractionDigits: 2,
+			});
+			return intl.format(amount);
+		},
+	},
+	computed: {
+		...mapGetters({
+			selectedNetworks: "getSelectedNetworks",
+			totalValue: "getTotalValue",
+			currency: "getCurrency",
+			balances: "getBalancesByName",
+			networksLoaded: "getIsNetworksLoaded",
+			balancesLoaded: "getIsBalancesLoaded",
+			seedAddresses: "getSeedAddresses",
+			showAlert: "getDashboardAlert",
+			balanceTime: "getBalanceExpireTime",
+			priceTime: "getLastPriceTime",
+			selectedWallet: "getSelectedWallet",
+		}),
+		mobile() {
+			console.log(this.$vuetify.display.sm);
+			return this.$vuetify.display.xs || this.$vuetify.display.sm;
+		},
+	},
 };
 </script>
 
 <style scoped>
 .code {
-  background-color: #f5f5f5;
+	background-color: #f5f5f5;
 }
 </style>
